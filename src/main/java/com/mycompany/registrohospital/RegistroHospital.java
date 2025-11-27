0f19017b872834e2eb81a53c604b23ea015ee3f2
Legion <Legion@192.168.56.1>
2025-11-27 18:49:32 -0300

    ASDF

--- src/main/java/com/mycompany/registrohospital/RegistroHospital.java
+++ src/main/java/com/mycompany/registrohospital/RegistroHospital.java
@@ -11,6 +11,7 @@ package com.mycompany.registrohospital;
 public class RegistroHospital {
 
     public static void main(String[] args) {
+        System.out.println("Pan de miga");
         System.out.println("Hello World!");
         System.out.println("Hello World!");
     }

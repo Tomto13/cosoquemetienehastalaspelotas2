88ee48d1c452d9a34873f582f0af07ef23556f60
Legion <Legion@192.168.56.1>
2025-11-27 18:31:26 -0300

    La cosa

---
+++ src/main/java/com/mycompany/registrohospital/RegistroHospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrohospital;
+
+/**
+ *
+ * @author Legion
+ */
+public class RegistroHospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

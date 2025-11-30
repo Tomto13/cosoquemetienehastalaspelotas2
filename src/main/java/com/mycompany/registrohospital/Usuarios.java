859b7d28a8950d44b3d28037fd5023f2287a3041
Legion <Legion@192.168.56.1>
2025-11-29 23:26:13 -0300

    nose que poner, son las 23:26 Usuarios

--- src/main/java/com/mycompany/registrohospital/Usuarios.java
+++ src/main/java/com/mycompany/registrohospital/Usuarios.java
@@ -2,86 +2,122 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.registrohospital;
+package main.java.com.mycompany.registrohospital;
 
 /**
  *
  * @author Legion
  */
 public class Usuarios {
+    private int id_paciente;
+    private String  rut;
+    private String nombre;
+    private String apellido;
+    private int  edad;
+    private String motivo;
+    private String fecha;
+    private String pasillo;
+    private String estado;
+    private String comentario;
+    private String contrasena;
     private int id_usuario;
-    private String  rutusuario;
-    private String nombreusuario;
-    private String apellidousuario;
-    private String  tipo_usuario;
-    private String pasillo_asignado;
-    private String contrasenausuario;
 
-    public Usuarios(int id_usuario, String rutusuario, String nombreusuario, String apellidousuario, String tipo_usuario, String pasillo_asignado, String contrasenausuario) {
-        this.id_usuario = id_usuario;
-        this.rutusuario = rutusuario;
-        this.nombreusuario = nombreusuario;
-        this.apellidousuario = apellidousuario;
-        this.tipo_usuario = tipo_usuario;
-        this.pasillo_asignado = pasillo_asignado;
-        this.contrasenausuario = contrasenausuario;
+    public Usuarios(String rut, String nombre, String apellido, int edad, String motivo, String fecha, String pasillo, String estado, String contrasena) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.motivo = motivo;
+        this.fecha = fecha;
+        this.pasillo = pasillo;
+        this.estado = estado;
+        this.comentario = "Sin observaciones";
+        this.contrasena = contrasena;
     }
 
-    public int getId_usuario() {
-        return id_usuario;
+    public String getRut() {
+        return rut;
     }
 
-    public String getRutusuario() {
-        return rutusuario;
+    public String getNombre() {
+        return nombre;
     }
 
-    public String getNombreusuario() {
-        return nombreusuario;
+    public String getApellido() {
+        return apellido;
     }
 
-    public String getApellidousuario() {
-        return apellidousuario;
+    public int getEdad() {
+        return edad;
     }
 
-    public String getTipo_usuario() {
-        return tipo_usuario;
+    public String getMotivo() {
+        return motivo;
     }
 
-    public String getPasillo_asignado() {
-        return pasillo_asignado;
+    public String getFecha() {
+        return fecha;
     }
 
-    public String getContrasenausuario() {
-        return contrasenausuario;
+    public String getPasillo() {
+        return pasillo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setPasillo(String pasillo) {
+        this.pasillo = pasillo;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
     }
 
     public void setId_usuario(int id_usuario) {
         this.id_usuario = id_usuario;
     }
-
-    public void setRutusuario(String rutusuario) {
-        this.rutusuario = rutusuario;
-    }
-
-    public void setNombreusuario(String nombreusuario) {
-        this.nombreusuario = nombreusuario;
-    }
-
-    public void setApellidousuario(String apellidousuario) {
-        this.apellidousuario = apellidousuario;
-    }
-
-    public void setTipo_usuario(String tipo_usuario) {
-        this.tipo_usuario = tipo_usuario;
-    }
-
-    public void setPasillo_asignado(String pasillo_asignado) {
-        this.pasillo_asignado = pasillo_asignado;
-    }
-
-    public void setContrasenausuario(String contrasenausuario) {
-        this.contrasenausuario = contrasenausuario;
-    }
-    
     
 }
+    
